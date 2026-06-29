ba356a31c6da9948581a02174116f282f8c1ac46
yarikthe5 <153199325+yarikthe5@users.noreply.github.com>
2026-06-29 18:49:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,46 +4,69 @@ import { WebSocketServer } from "ws";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Yarchat Fast is running 🚀");
-});
+app.use(express.static("public"));
 
 const server = app.listen(port, () => {
-  console.log("Server started on port " + port);
+  console.log("Yarchat Fast V2 running on " + port);
 });
 
 const wss = new WebSocketServer({ server });
 
-// просте сховище повідомлень (для 2 людей достатньо)
+let clients = {};
 let messages = [];
 
 wss.on("connection", (ws) => {
-  console.log("Client connected");
+  let user = "anon";
 
-  // відправляємо історію
+  ws.on("message", (data) => {
+    const msg = JSON.parse(data.toString());
+
+    // login
+    if (msg.type === "login") {
+      user = msg.name;
+      clients[user] = ws;
+
+      broadcast({
+        type: "system",
+        text: `${user} joined chat`
+      });
+    }
+
+    // chat message
+    if (msg.type === "message") {
+      const message = {
+        user,
+        text: msg.text,
+        time: Date.now()
+      };
+
+      messages.push(message);
+
+      broadcast({
+        type: "message",
+        data: message
+      });
+    }
+
+    // typing
+    if (msg.type === "typing") {
+      broadcast({
+        type: "typing",
+        user
+      });
+    }
+  });
+
   ws.send(JSON.stringify({
     type: "history",
     data: messages
   }));
-
-  ws.on("message", (data) => {
-    const msg = data.toString();
-
-    const message = {
-      text: msg,
-      time: Date.now()
-    };
-
-    messages.push(message);
-
-    // розсилаємо всім
-    wss.clients.forEach(client => {
-      if (client.readyState === 1) {
-        client.send(JSON.stringify({
-          type: "message",
-          data: message
-        }));
-      }
-    });
-  });
 });
+
+function broadcast(data) {
+  wss.clients.forEach(client => {
+    if (client.readyState === 1) {
+      client.send(JSON.stringify(data));
+    }
+  });
+}
